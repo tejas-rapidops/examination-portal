cd70d14270577ca85f82eb369ae7fcc5797cd89c
tejas-rapidops <tejas.vora@rapidops.com>
2019-10-09 18:49:30 +0530

    project setup.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,85 +1,41 @@
 module.exports = {
-  /**
-   * Application configuration section
-   * http://pm2.keymetrics.io/docs/usage/application-declaration/
-   * pm2 deploy ecosystem.config.js dev setup
-   * pm2 deploy ecosystem.config.js dev update
-   * pm2 deploy dev update
-   * pm2 deploy dev exec "pm2 restart all"
-   */
-  apps: [
-    {
-      name: 'development',
-      script: 'index.js',
-      exec_mode: 'fork',
-    },
-    {
-      name: 'qa',
-      script: 'index.js',
-      exec_mode: 'cluster',
-    },
-    {
-      name: 'production',
-      script: 'index.js',
-      exec_mode: 'cluster',
-    }
-  ],
+  apps : [{
+    name: 'API',
+    script: 'npm start',
 
-  /**
-   * Deployment section
-   * http://pm2.keymetrics.io/docs/usage/deployment/
-   */
-  deploy: {
-    dev: {
-      name: 'development',
-      user: 'examination-portal',
-      host: '52.0.219.227',
-      ref: 'origin/development',
-      repo: 'git@github.com:tejas-rapidops/examination-portal.git',
-      path: '/home/krogerfresh/app',
-      'pre-deploy-local': '',
-      'post-deploy': 'cd /home/krogerfresh/app/current; ' +
-        'kill -9 $(/usr/sbin/lsof -t -i:9005 -sTCP:LISTEN); ' +
-        'pkill node; ' +
-        'npm install --only=production; ' +
-        'NODE_ENV=dev node --inspect=172.31.1.84:4444 index.js > log.txt &',
-      env: {
-        NODE_ENV: 'dev',
-      },
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    args: 'npm run build',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
     },
-    qa: {
-      name: 'qa',
-      user: 'examination-portal',
-      host: '52.0.219.227',
-      ref: 'origin/qa-build',
-      repo: 'git@github.com:tejas-rapidops/examination-portal.git',
-      path: '/home/examination-portal',
-      'post-deploy': 'cd /home/krogerfresh/app/current; ' +
-        'kill -9 $(/usr/sbin/lsof -t -i:9005 -sTCP:LISTEN); ' +
-        'pkill node; ' +
-        'npm install --only=production; ' +
-        'NODE_ENV=qa node --inspect=172.31.1.84:4444 index.js > log.txt &',
-      env: {
-        NODE_ENV: 'qa',
-      },
+    env_dev: {
+      NODE_ENV: 'dev'
     },
-    production: {
-      name: 'production',
-      user: 'examination-portal',
-      host: '52.0.219.227',
-      ref: 'origin/production-build',
-      repo: 'git@github.com:tejas-rapidops/examination-portal.git',
-      path: '/home/examination-portal',
-      'post-deploy': 'cd /home/krogerfresh/app/current; ' +
-        'kill -9 $(/usr/sbin/lsof -t -i:9002 -sTCP:LISTEN); ' +
-        'pkill node; ' +
-        'npm install --only=production; ' +
-        'NODE_ENV=production node --inspect=172.31.1.84:4444 index.js > log.txt &',
-      env: {
-        NODE_ENV: 'production',
-      },
+    env_production: {
+      NODE_ENV: 'production'
     }
-  },
+  }],
+  deploy : {
+    dev : {
+      user : 'examination-portal',
+      host : '52.0.219.227',
+      ref  : 'origin/development',
+      repo : 'git@github.com:tejas-rapidops/examination-portal.git',
+      path : '/home/examination-portal/current',
+      'pre-deploy':'mkdir current',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    },
+    production : {
+      user : 'node',
+      host : '212.83.163.1',
+      ref  : 'origin/master',
+      repo : 'git@github.com:repo.git',
+      path : '/var/www/production',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
 };
-
-
