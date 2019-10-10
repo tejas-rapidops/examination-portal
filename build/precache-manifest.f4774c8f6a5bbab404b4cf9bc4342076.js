16b99d3385c68b49c2de452c3b64fee310d151d5
tejas-rapidops <tejas.vora@rapidops.com>
2019-10-10 19:18:44 +0530

    project setup.

---
+++ build/precache-manifest.f4774c8f6a5bbab404b4cf9bc4342076.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fe196fdb824d2d5bb18aac3dad00f05",
+    "url": "/tejas-rapidops/examination-portal/index.html"
+  },
+  {
+    "revision": "30fb3ec71bb6a3212f49",
+    "url": "/tejas-rapidops/examination-portal/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "442a6bafe9859246fd85",
+    "url": "/tejas-rapidops/examination-portal/static/js/2.1c128480.chunk.js"
+  },
+  {
+    "revision": "30fb3ec71bb6a3212f49",
+    "url": "/tejas-rapidops/examination-portal/static/js/main.0c69bb22.chunk.js"
+  },
+  {
+    "revision": "dd67f5edc0d99c5e3c13",
+    "url": "/tejas-rapidops/examination-portal/static/js/runtime-main.eaec0947.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/tejas-rapidops/examination-portal/static/media/logo.25bf045c.svg"
+  }
+]);
